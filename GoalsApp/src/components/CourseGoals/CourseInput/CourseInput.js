d0426a62a865fb83ed43cb3c419d835016c2eba3
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-08-20 18:23:43 +0300

    Adding className dynamically
    Changing from inline styles to className changes dynamically

--- GoalsApp/src/components/CourseGoals/CourseInput/CourseInput.js
+++ GoalsApp/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -18,7 +18,7 @@ const CourseInput = props => {
     event.preventDefault();
     if (enteredValue.trim().length == 0) {
       setIsValid(false);
-      return
+      return;
     };
 
     props.onAddGoal(enteredValue);
@@ -27,9 +27,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
-        <input style={{ borderColor: !isValid ? 'red' : '#ccc', background: !isValid ? 'salmon' : 'transparent' }} type="text" value={enteredValue} onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
+        <input type="text" value={enteredValue} onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
