42c370ea496116473eb1da54de404f43c8369b43
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-08-24 13:39:04 +0300

    Making components
    Adding InvestmentInput and Button components

---
+++ InvestmentCalculator/src/components/InvestmentInput/InvestmentInput.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+
+const InvestmentInput = props => {
+    return (
+        <form className="form">
+            <div className="input-group">
+                <p>
+                    <label htmlFor="current-savings">Current Savings ($)</label>
+                    <input type="number" id="current-savings" />
+                </p>
+                <p>
+                    <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+                    <input type="number" id="yearly-contribution" />
+                </p>
+            </div>
+            <div className="input-group">
+                <p>
+                    <label htmlFor="expected-return">
+                        Expected Interest (%, per year)
+                    </label>
+                    <input type="number" id="expected-return" />
+                </p>
+                <p>
+                    <label htmlFor="duration">Investment Duration (years)</label>
+                    <input type="number" id="duration" />
+                </p>
+            </div>
+            // add button here
+        </form>
+    )
+};
+
+export default InvestmentInput;
---
+++ InvestmentCalculator/src/components/UI/Button/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Button.css';
+
+const Button = props => {
+    <p className="actions">
+        <button type="reset" className="buttonAlt">
+            Reset
+        </button>
+        <button type="submit" className="button">
+            Calculate
+        </button>
+    </p>
+}
