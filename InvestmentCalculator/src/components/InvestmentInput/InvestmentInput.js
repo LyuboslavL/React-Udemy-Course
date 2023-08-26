efb91c069a1eab641a8c62d08b6576c5b982a0c4
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-08-26 16:38:17 +0300

    Building the InvestmentInput component
    Adding functionality and useState

--- InvestmentCalculator/src/components/InvestmentInput/InvestmentInput.js
+++ InvestmentCalculator/src/components/InvestmentInput/InvestmentInput.js
@@ -10,6 +10,11 @@ const InvestmentInput = props => {
     const [enteredInterest, setEnteredInterest] = useState('');
     const [enteredDuration, setEnteredDuration] = useState('');
 
+    const currentSavingsHandler = (event) => setEnteredCurrent(event.target.value);
+    const yearlySavingsHandler = (event) => setEnteredYearly(event.target.value);
+    const expectedInterestHandler = (event) => setEnteredInterest(event.target.value);
+    const investmentDurationHandler = (event) => setEnteredDuration(event.target.value);
+
     const formSubmitHandler = event => {
         event.preventDefault();
 
@@ -28,11 +33,11 @@ const InvestmentInput = props => {
             <div className="input-group">
                 <p>
                     <label htmlFor="current-savings">Current Savings ($)</label>
-                    <input type="number" id="current-savings" />
+                    <input type="number" id="current-savings" onChange={currentSavingsHandler} />
                 </p>
                 <p>
                     <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-                    <input type="number" id="yearly-contribution" />
+                    <input type="number" id="yearly-contribution" onChange={yearlySavingsHandler} />
                 </p>
             </div>
             <div className="input-group">
@@ -40,11 +45,11 @@ const InvestmentInput = props => {
                     <label htmlFor="expected-return">
                         Expected Interest (%, per year)
                     </label>
-                    <input type="number" id="expected-return" />
+                    <input type="number" id="expected-return" onChange={expectedInterestHandler} />
                 </p>
                 <p>
                     <label htmlFor="duration">Investment Duration (years)</label>
-                    <input type="number" id="duration" />
+                    <input type="number" id="duration" onChange={investmentDurationHandler} />
                 </p>
             </div>
             <p className="actions">
