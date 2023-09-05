7ce89907beae7f6046a6b556ae64b6051cb70d93
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-09-05 17:34:21 +0300

    Adjusting styles
    Adding to AddUser, Button and index components

--- UserList/src/components/AddUser/AddUser.js
+++ UserList/src/components/AddUser/AddUser.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
+import './AddUser.css';
 import Button from '../../components/UI/Button/Button';
 
 const AddUser = (props) => {
     return (
         <form>
-            <div>
-                <label>Username</label>
-                <input type="text"></input>
-                <label>Age (Years)</label>
-                <input type='text'></input>
+            <div className='add-user'>
+                <div className='add-user__input'>
+                    <label>Username</label>
+                    <input type="text"></input>
+                </div>
+                <div className='add-user__input'>
+                    <label>Age (Years)</label>
+                    <input type='text'></input>
+                </div>
+                <div className='add_user__action'>
+                    <Button type='submit'>Add User</Button>
+                </div>
             </div>
-            <Button type='submit'>Add User</Button>
         </form>
     )
 };
--- UserList/src/components/UI/Button/Button.js
+++ UserList/src/components/UI/Button/Button.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './Button.css';
+
 const Button = (props) => {
     return (
         <button type={props.type} onClick={props.onClick}>{props.children}</button>
