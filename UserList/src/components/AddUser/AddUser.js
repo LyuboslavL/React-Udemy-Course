1cc2043ac43d18e9075fd0e93ce8b0d09dd42244
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-09-05 17:39:15 +0300

    useState when adding user
    Adding it to the AddUser component

--- UserList/src/components/AddUser/AddUser.js
+++ UserList/src/components/AddUser/AddUser.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './AddUser.css';
 import Button from '../../components/UI/Button/Button';
 
 const AddUser = (props) => {
+    const [enteredUsername, setUsername] = useState('');
+    const [enteredAge, setAge] = useState('');
+
+    const usernameChangeHandler = (event) => setUsername(event.target.value);
+    const ageChangeHandler = (event) => setAge(event.target.value);
+
+    const formSubmitHandler = event => {
+        event.preventDefault();
+        const newUser = {
+            username: enteredUsername,
+            age: enteredAge
+        };
+
+        console.log(newUser);
+    }
+
     return (
-        <form>
+        <form onSubmit={formSubmitHandler}>
             <div className='add-user'>
                 <div className='add-user__input'>
                     <label>Username</label>
-                    <input type="text"></input>
+                    <input type="text" onChange={usernameChangeHandler}></input>
                 </div>
                 <div className='add-user__input'>
                     <label>Age (Years)</label>
-                    <input type='text'></input>
+                    <input type='text' onChange={ageChangeHandler}></input>
                 </div>
                 <div className='add_user__action'>
                     <Button type='submit'>Add User</Button>
