fc7872c3066272ddebf950b1b0cd43df5f043ab1
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-09-08 22:15:25 +0300

    Functionality to ErrorModal
    Adding basic things

--- UserList/src/components/ErrorModal/ErrorModal.js
+++ UserList/src/components/ErrorModal/ErrorModal.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import Button from '../UI/Button/Button'
+
 const ErrorModal = props => {
-    return ()
+    return (
+        <section>
+            <header>
+                <h2>Error!</h2>
+            </header>
+            <div>
+                <p>{props.children}</p>
+            </div>
+            <footer>
+                <Button>Close</Button>
+            </footer>
+        </section>
+    )
 };
 
 export default ErrorModal;
