3d0257bd8a90ad2c014cb2f942c6a3d4315f0334
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-09-11 21:06:27 +0300

    Styling the UserList
    Adding styles to the UserList component.

--- UserList/src/components/Card/Card.js
+++ UserList/src/components/Card/Card.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './Card.css'
+
 const Card = (props) => {
     return (
-        <li className='user-card'>
+        <li>
             {props.children}
         </li>
     );
