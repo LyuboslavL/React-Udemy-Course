f359496f9740032087dee9a9837692c998c7a219
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-09-17 13:10:58 +0300

    Creating Portal
    First steps.

--- UserList/src/components/UI/ErrorModal/ErrorModal.js
+++ UserList/src/components/UI/ErrorModal/ErrorModal.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import Button from '../Button/Button';
 import classes from './ErrorModal.module.css';
 
+const Backdrop = props => {
+    return (
+        <div className={classes.backdrop} onClick={props.onConfirm} />
+    )
+};
+
+const ModalOverlay = props => {
+    <section className={classes.modal}>
+        <header className={classes.header}>
+            <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+            <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+            <Button onClick={props.onConfirm}>Close</Button>
+        </footer>
+    </section>
+};
+
 const ErrorModal = props => {
     return (
         <React.Fragment>
-            <div className={classes.backdrop} onClick={props.onConfirm} />
-            <section className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onConfirm}>Close</Button>
-                </footer>
-            </section>
+            {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />, document.getElementById('backdrop-root'))}
         </React.Fragment>
     )
 };
