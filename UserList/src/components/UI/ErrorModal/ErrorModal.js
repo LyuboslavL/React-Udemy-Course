0e35a7a9332b3f7c2f3bf1ec6d7d192d803653c9
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-09-17 20:59:05 +0300

    Update ErrorModal
    Trying to make the portals work properly.

--- UserList/src/components/UI/ErrorModal/ErrorModal.js
+++ UserList/src/components/UI/ErrorModal/ErrorModal.js
@@ -11,23 +11,25 @@ const Backdrop = props => {
 };
 
 const ModalOverlay = props => {
-    <section className={classes.modal}>
-        <header className={classes.header}>
-            <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-            <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-            <Button onClick={props.onConfirm}>Close</Button>
-        </footer>
-    </section>
+
 };
 
 const ErrorModal = props => {
     return (
         <React.Fragment>
             {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />, document.getElementById('backdrop-root'))};
+            {/* {ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm} />, document.getElementById('overlay-root'))} */}
+            <section className={classes.modal}>
+                <header className={classes.header}>
+                    <h2>{props.title}</h2>
+                </header>
+                <div className={classes.content}>
+                    <p>{props.message}</p>
+                </div>
+                <footer className={classes.actions}>
+                    <Button onClick={props.onConfirm}>Close</Button>
+                </footer>
+            </section>
         </React.Fragment>
     )
 };
