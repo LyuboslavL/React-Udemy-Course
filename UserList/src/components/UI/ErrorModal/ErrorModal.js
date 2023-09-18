e170f5eff17d3359bf1dd0e8c862da9330da54a4
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-09-18 20:04:32 +0300

    Working ErrorModal
    Separating it from the root.

--- UserList/src/components/UI/ErrorModal/ErrorModal.js
+++ UserList/src/components/UI/ErrorModal/ErrorModal.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Button from '../Button/Button';
 import classes from './ErrorModal.module.css';
+import Wrapper from '../../Helpers/Wrapper';
 
 const Backdrop = props => {
     return (
@@ -10,26 +11,27 @@ const Backdrop = props => {
     )
 };
 
-const ModalOverlay = props => {
-
+const ModalOverlay = (props) => {
+    return (
+        <section className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={classes.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={classes.actions}>
+                <Button onClick={props.onConfirm}>Close</Button>
+            </footer>
+        </section>
+    )
 };
 
 const ErrorModal = props => {
     return (
         <React.Fragment>
             {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />, document.getElementById('backdrop-root'))};
-            {/* {ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm} />, document.getElementById('overlay-root'))} */}
-            <section className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onConfirm}>Close</Button>
-                </footer>
-            </section>
+            {ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm} />, document.getElementById('overlay-root'))}
         </React.Fragment>
     )
 };
