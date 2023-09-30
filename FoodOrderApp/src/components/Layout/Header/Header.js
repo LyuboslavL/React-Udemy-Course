3212e4c454b7a172449eafcfad782fad16e56dca
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-09-30 18:26:35 +0300

    Creating a Header component
    With a cart button.

---
+++ FoodOrderApp/src/components/Layout/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+
+import mainImg from '../../../assets/main-image.jpg';
+import classes from './Header.module.css';
+
+const Header = () => {
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>finger lickin' good...</h1>
+                <HeaderCartButton />
+            </header>
+            <div>
+                <img className={classes['main-image']} src={mainImg} alt='A banner of a restaurant with healthy meals' />
+            </div>
+        </>
+    )
+};
+
+export default Header;
