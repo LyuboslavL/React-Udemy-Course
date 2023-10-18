64665a41493ee62a1cd101b0115e073dff5be66f
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2023-10-18 16:45:15 +0300

    Making the Add button works
    Updating the cart on top when button is pressed and a valid number is entered.

--- FoodOrderApp/src/components/Meals/MealItemForm.js
+++ FoodOrderApp/src/components/Meals/MealItemForm.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 import classes from './MealItemForm.module.css';
 import Input from '../UI/Input';
 
 const MealItemForm = props => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = Number(enteredAmount);
+
+        if (enteredAmount.trim().length === 0 || enteredAmountNumber < 0 || enteredAmountNumber > 20) {
+            setAmountIsValid(false);
+            return;
+        };
+
+        props.onAddToCart(enteredAmountNumber);
+
+    }
+
     return (
-        <form className={classes.form}>
-            <Input label='Amount' input={{
-                id: 'amount_' + props.id,
-                type: 'number',
-                min: '1',
-                max: '20',
-                step: '1'
-            }} />
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input
+                ref={amountInputRef}
+                label='Amount'
+                input={{
+                    id: 'amount_' + props.id,
+                    type: 'number',
+                    min: '1',
+                    max: '20',
+                    step: '1'
+                }} />
             <button>Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-20).</p>}
         </form>
     )
 };
