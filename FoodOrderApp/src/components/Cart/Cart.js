1d7844e168b9fc3dd0833c9737ed257e473efdc7
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-10-19 15:13:03 +0300

    Update Cart.js
    Calculating the sum depending on items added and their price.

--- FoodOrderApp/src/components/Cart/Cart.js
+++ FoodOrderApp/src/components/Cart/Cart.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
+import CartContext from '../../store/cart-context';
 
 const Cart = props => {
-    const cartItems = [];
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount = cartCtx.totalAmount.toFixed(2);
+    console.log(cartCtx);
+
+    const cartItems = (
+        <ul className={classes['cart-items']}>
+            {cartCtx.items.map((item) => {
+                console.log(item.name);
+                <li>{item.name}</li>
+            })}
+        </ul>
+    );
 
     return (
         <Modal onCancel={props.onCancel}>
+            {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>$3.99</span>
+                <span>${totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes['button--alt']} onClick={props.onCancel}>Close</button>
