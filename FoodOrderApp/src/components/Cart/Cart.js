78646ff689db5e61922f951a07e61fb650785bb5
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-10-22 09:46:31 +0300

    Update Cart component for better readability
    Update Cart component for better readability.

--- FoodOrderApp/src/components/Cart/Cart.js
+++ FoodOrderApp/src/components/Cart/Cart.js
@@ -21,7 +21,13 @@ const Cart = props => {
     const cartItems = (
         <ul className={classes['cart-items']}>
             {cartCtx.items.map((item) => {
-                return <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)} />
+                return <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)} />
             })}
         </ul>
     );
