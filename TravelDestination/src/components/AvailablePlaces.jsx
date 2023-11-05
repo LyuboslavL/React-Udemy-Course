91bda7da1cf3d23bcd310bbca2c8aca67a4e0fc5
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-11-05 21:11:33 +0200

    display loading text
    Based on the status of the data fatching.

--- TravelDestination/src/components/AvailablePlaces.jsx
+++ TravelDestination/src/components/AvailablePlaces.jsx
@@ -2,13 +2,16 @@ import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFatching, setIsFatching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFatching(true);
       const response = await fetch('http://localhost:3000/places');
       const data = await response.json();
       setAvailablePlaces(data.places); 
+      setIsFatching(false);
     }
 
     fetchPlaces();
@@ -18,6 +21,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFatching}
+      loadingText="Loading places..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
