068334c12d35e11753817692846ea5833f92637e
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-11-06 15:25:54 +0200

    error handling
    Showing a message in case an error occurs.

--- TravelDestination/src/components/AvailablePlaces.jsx
+++ TravelDestination/src/components/AvailablePlaces.jsx
@@ -1,21 +1,38 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFatching, setIsFatching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFatching(true);
-      const response = await fetch('http://localhost:3000/places');
-      const data = await response.json();
-      setAvailablePlaces(data.places); 
+      
+      try {
+        const response = await fetch('http://localhost:3000/placessss');
+        const data = await response.json();
+        
+        if (!response.ok) {
+          throw new Error('Oops, something went wrong :/');
+        };
+        
+        setAvailablePlaces(data.places); 
+      } catch (error) {
+        setError({message: error.message || 'Error occured'});
+      }
+
       setIsFatching(false);
     }
 
     fetchPlaces();
-  }, [])
+  }, []);
+
+  if (error) {
+    return <Error title="Ooops, something went wrong..." message={error.message}/>
+  }
 
   return (
     <Places
