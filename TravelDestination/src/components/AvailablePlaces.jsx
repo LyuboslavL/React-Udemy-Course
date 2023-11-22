b4c66fb98e11dc76859024509c111a1b20623456
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-11-22 19:55:11 +0200

    making the custom hook flexible
    Adding the sorting by location on the AvailablePlaces component by using the custom hook.

--- TravelDestination/src/components/AvailablePlaces.jsx
+++ TravelDestination/src/components/AvailablePlaces.jsx
@@ -1,41 +1,34 @@
-import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 import sortPlacesByDistance from "../loc.js";
 import { fetchAvailablePlaces } from "../http.js";
 import { useFetch } from "../hooks/useFetch.js";
 
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+
+      resolve(sortedPlaces);
+    });
+  });
+}
+
 export default function AvailablePlaces({ onSelectPlace }) {
   const {
     isLoading,
     error,
     fetchedData: availablePlaces,
-    setFetchedData: setAvailablePlaces,
-  } = useFetch(fetchAvailablePlaces, []);
-
-  // useEffect(() => {
-  //   async function fetchPlaces() {
-  //     setIsFetching(true);
-
-  //     try {
-  //       const places = await fetchAvailablePlaces();
-
-  //       navigator.geolocation.getCurrentPosition((position) => {
-  //         const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude);
-  //         setAvailablePlaces(sortedPlaces);
-  //         setIsFetching(false);
-  //       });
-
-  //     } catch (error) {
-  //       setError({ message: error.message || 'Error occured' });
-  //       setIsFetching(false);
-  //     }
-  //   }
-
-  //   fetchPlaces();
-  // }, []);
+  } = useFetch(fetchSortedPlaces, []);
 
   if (error) {
+    console.log(error);
     return (
       <Error title="Ooops, something went wrong..." message={error.message} />
     );
