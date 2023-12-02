b03e9c01b32339191f1dfdb880409cbef58ffcf6
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2023-12-02 19:35:24 +0200

    added return on custom hook
    useInput to return state and functions.

--- Forms/src/hooks/useInput.js
+++ Forms/src/hooks/useInput.js
@@ -1,10 +1,21 @@
 import { useState } from "react";
 
-export function useInput() {
-    const [enteredValue, setEnteredValue] = useState();
+export function useInput(defaultValue) {
+    const [enteredValue, setEnteredValue] = useState(defaultValue);
+    const [didEdit, setDidEdit] = useState(false);
 
-    const [didEdit, setDidEdit] = useState({
-        email: false,
-        password: false,
-    });
+    const handleInputChange = (event) => {
+        setEnteredValue(event.target.value);
+        setDidEdit(false);
+    };
+
+    const handleInputBlur = (identifier) => {
+        setDidEdit(true);
+    };
+
+    return {
+        value: enteredValue,
+        handleInputBlur,
+        handleInputChange
+    }
 } 
