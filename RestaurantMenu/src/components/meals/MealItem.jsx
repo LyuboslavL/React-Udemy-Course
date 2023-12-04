a86864c45beea84711b931b035bef504a1ac1f00
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2023-12-04 14:52:38 +0200

    adding components for meals displaying
    MealItem and AvailableMeals.

---
+++ RestaurantMenu/src/components/meals/MealItem.jsx
@@ -0,0 +1,17 @@
+const MealItem = (props) => {
+  return (
+    <li>
+      <div>
+        <article>
+          <img></img>
+          <h3>Dummy meal</h3>
+          <div>Description</div>
+          <div>Price</div>
+          <div>Actions</div>
+        </article>
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
