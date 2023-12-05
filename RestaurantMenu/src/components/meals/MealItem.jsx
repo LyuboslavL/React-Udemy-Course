4abd2d39c920f30ab5d604eb0978b9c39f29eba1
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2023-12-05 15:18:48 +0200

    creating Button component
    For reusability.

--- RestaurantMenu/src/components/meals/MealItem.jsx
+++ RestaurantMenu/src/components/meals/MealItem.jsx
@@ -1,3 +1,5 @@
+import Button from "../ui/Button.jsx";
+
 import classes from "./MealItem.module.css";
 
 const MealItem = ({ name, image, description, price, actions }) => {
@@ -9,9 +11,10 @@ const MealItem = ({ name, image, description, price, actions }) => {
           <h3>{name}</h3>
           <div className={classes["meal-item-price"]}>${price}</div>
           <div className={classes["meal-item-description"]}>{description}</div>
-          <div className={classes["meal-item-actions"]}>
-            <button className={classes.button}>Add to Cart</button>
-          </div>
+          {/* <div className={classes["meal-item-actions"]}> */}
+          <Button className="-actions">Add to Cart</Button>
+          {/* <button className={classes.button}>Add to Cart</button> */}
+          {/* </div> */}
         </div>
       </article>
     </li>
