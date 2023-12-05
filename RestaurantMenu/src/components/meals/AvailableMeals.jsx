101def115fa999996c47dcb72c9ecba7173f8f56
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2023-12-05 12:11:51 +0200

    styling the components
    Removing unnecessary "console.log" in the components.

--- RestaurantMenu/src/components/meals/AvailableMeals.jsx
+++ RestaurantMenu/src/components/meals/AvailableMeals.jsx
@@ -2,9 +2,11 @@ import MealItem from "./MealItem.jsx";
 import { fetchAvailableMeals } from "../../../http.js";
 import { useFetch } from "../hooks/useFetch.js";
 
+import classes from "./AvailableMeals.module.css";
+
 async function fetchMeals() {
   const meals = fetchAvailableMeals();
-  console.log(meals);
+
   return new Promise((resolve) => {
     resolve(meals);
   });
@@ -17,12 +19,10 @@ const AvailableMeals = (props) => {
     fetchedData: availableMeals,
   } = useFetch(fetchMeals, []);
 
-  console.log(availableMeals);
-
   // if (error) {return (<Error title="Something went wrong." message={error.message}/>)}
 
   return (
-    <ul>
+    <ul id={classes.meals}>
       {availableMeals.map((meal) => (
         <MealItem
           id={meal.id}
