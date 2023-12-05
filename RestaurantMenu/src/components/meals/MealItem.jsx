5a07fac34e8560446214d902e1038dc81a5b90b1
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2023-12-05 14:34:16 +0200

    styling on MealItem
    Better composition and styling.

--- RestaurantMenu/src/components/meals/MealItem.jsx
+++ RestaurantMenu/src/components/meals/MealItem.jsx
@@ -3,15 +3,17 @@ import classes from "./MealItem.module.css";
 const MealItem = ({ name, image, description, price, actions }) => {
   return (
     <li>
-      <div className={classes["meal-item"]}>
-        <article>
+      <article>
+        <div className={classes["meal-item"]}>
           <img src={`http://localhost:3000/${image}`}></img>
           <h3>{name}</h3>
-          <div className={classes.description}>{description}</div>
-          <div className={classes.price}>${price}</div>
-          <div className={classes.actions}>Actions</div>
-        </article>
-      </div>
+          <div className={classes["meal-item-price"]}>${price}</div>
+          <div className={classes["meal-item-description"]}>{description}</div>
+          <div className={classes["meal-item-actions"]}>
+            <button className={classes.button}>Add to Cart</button>
+          </div>
+        </div>
+      </article>
     </li>
   );
 };
