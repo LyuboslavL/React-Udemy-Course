bb8e7d0605196e6502e51045ba3d6d05de455a1c
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2023-12-08 16:06:26 +0200

    backdrop
    Working on the overlay and trying to place it behind the cart.

--- RestaurantMenu/src/components/ui/Modal.jsx
+++ RestaurantMenu/src/components/ui/Modal.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  return <div className={classes.overlay} onClick={props.onCancel} />;
+  return <div className={classes.backdrop} onClick={props.onCancel} />;
 };
 
 const ModalOverlay = (props) => {
