752e22a6b06b844aa7c4c49daa56c653f08af7f9
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2023-12-18 15:11:11 +0200

    Modal size
    Depending on content inside.

--- RestaurantMenu/src/components/cart/Cart.jsx
+++ RestaurantMenu/src/components/cart/Cart.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 
 import classes from "./Cart.module.css";
 
@@ -9,14 +9,7 @@ import CartContext from "../../store/cart-context.jsx";
 import { currencyFormatter } from "../../util/formatter.js";
 
 const Cart = (props) => {
-  // const [cartIsEmpty, setCartIsEmpty] = useState(true);
   const cartCtx = useContext(CartContext);
-
-  // if (cartCtx.items.length === 0) {
-  //   setCartIsEmpty(true);
-  // } else {
-  //   setCartIsEmpty(false);
-  // }
 
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
@@ -35,20 +28,24 @@ const Cart = (props) => {
     <Modal onCancel={props.onCancel}>
       <div className={classes.cart}>
         <h2>Your Cart</h2>
-        <ul>
-          {cartCtx.items.map((item) => {
-            return (
-              <CartItem
-                key={item.id}
-                name={item.name}
-                quantity={item.quantity}
-                price={item.price}
-                onRemove={cartItemRemoveHandler.bind(null, item.id)}
-                onAdd={cartItemAddHandler.bind(null, item)}
-              />
-            );
-          })}
-        </ul>
+        {cartCtx.items.length > 0 ? (
+          <ul>
+            {cartCtx.items.map((item) => {
+              return (
+                <CartItem
+                  key={item.id}
+                  name={item.name}
+                  quantity={item.quantity}
+                  price={item.price}
+                  onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                  onAdd={cartItemAddHandler.bind(null, item)}
+                />
+              );
+            })}
+          </ul>
+        ) : (
+          <p>No items in your cart yet. Why don't you add some?</p>
+        )}
         <p className={classes["cart-total"]}>
           {currencyFormatter.format(cartTotal)}
         </p>
@@ -56,7 +53,9 @@ const Cart = (props) => {
           <Button textOnly onClick={props.onCancel}>
             Close
           </Button>
-          <Button onClick={props.onOrder}>Go to Checkout</Button>
+          {cartCtx.items.length > 0 && (
+            <Button onClick={props.onOrder}>Go to Checkout</Button>
+          )}
         </p>
       </div>
     </Modal>
