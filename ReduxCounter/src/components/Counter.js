4ea35c8fdafeff80e5bc7fbdf7abbaa609a46e35
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2023-12-27 19:18:01 +0200

    Counter as a class-based component
    Using it with Redux.

--- ReduxCounter/src/components/Counter.js
+++ ReduxCounter/src/components/Counter.js
@@ -49,10 +49,10 @@ class Counter extends Component {
     return (
       <main className={classes.counter}>
         <h1>Redux Counter</h1>
-        <div className={classes.value}>{counter}</div>
+        <div className={classes.value}>{this.props.counter}</div>
         <div>
-          <button onClick={this.incrementHandler}>Increment</button>
-          <button onClick={this.decrementHandler}>Decrement</button>
+          <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+          <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
         </div>
         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
       </main>
