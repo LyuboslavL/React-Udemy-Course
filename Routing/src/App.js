90391ea4fad2cea867506d9410fc7a3789fa09ba
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2024-01-07 19:18:26 +0200

    adding a second route
    By using React Router.

--- Routing/src/App.js
+++ Routing/src/App.js
@@ -1,8 +1,10 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './components/Home';
+import Products from './components/Products';
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> }
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <Products /> }
 ])
 
 function App() {
---
+++ Routing/src/components/Products.js
@@ -0,0 +1,9 @@
+const Products = () => {
+    return (
+        <>
+            <h2>Products</h2>
+            <div>This is the page where all the products are showed.</div>
+        </>)
+};
+
+export default Products;
