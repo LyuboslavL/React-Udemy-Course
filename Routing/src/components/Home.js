0b252d317c073bee987e23a8ba57a25e9c5031d2
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2024-01-08 15:36:14 +0200

    using Link functionality
    To avoid sending a new HTTP request.

--- Routing/src/components/Home.js
+++ Routing/src/components/Home.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
-    return <h1>Homepage</h1>
-};
+  return (
+    <>
+      <h1>Homepage</h1>
+      <p>
+        Go to the <Link to="/products">Products</Link> page
+      </p>
+    </>
+  );
+}
 
 export default HomePage;
