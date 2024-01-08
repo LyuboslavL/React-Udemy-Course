ca3d53abdbc48df316e3bf4ce42156ebe94229c8
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2024-01-08 15:52:42 +0200

    styling and nested routes
    Using a Layout

--- Routing/src/App.js
+++ Routing/src/App.js
@@ -1,11 +1,18 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomePage from './components/Home';
-import Products from './components/Products';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Products from "./pages/ProductsPage";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element: <Products /> }
-])
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <Products /> },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
