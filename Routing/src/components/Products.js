826924cd5a736e8ea6ed88af79b47d9867265274
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2024-01-08 15:38:04 +0200

    navigation through the pages
    With React Router.

--- Routing/src/components/Products.js
+++ Routing/src/components/Products.js
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
-    return (
-        <>
-            <h2>Products</h2>
-            <div>This is the page where all the products are showed.</div>
-        </>)
+  return (
+    <>
+      <h2>Products</h2>
+      <div>This is the page where all the products are showed.</div>
+      <button>
+        <Link to="/">Homepage</Link>
+      </button>
+    </>
+  );
 };
 
 export default Products;
