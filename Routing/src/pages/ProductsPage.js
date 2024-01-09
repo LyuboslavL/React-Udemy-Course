ea5b6ccb7a3d7fbc670be6e43443da7f386b6436
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2024-01-09 15:43:36 +0200

    links for dynamic routes
    Adding links to the Products.

--- Routing/src/pages/ProductsPage.js
+++ Routing/src/pages/ProductsPage.js
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Products = () => {
   const navigate = useNavigate();
@@ -11,6 +11,17 @@ const Products = () => {
     <>
       <h2>Products</h2>
       <div>This is the page where all the products are showed.</div>
+      <ul>
+        <li>
+          <Link to="/products/book">Book</Link>
+        </li>
+        <li>
+          <Link to="/products/tickets">Tickets</Link>
+        </li>
+        <li>
+          <Link to="/products/food-voucher">Food voucher</Link>
+        </li>
+      </ul>
       <button onClick={navigateHandler}>Homepage</button>
       {/* <Link to="/">Homepage</Link> */}
     </>
