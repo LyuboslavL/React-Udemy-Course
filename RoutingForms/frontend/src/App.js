1b1f9bc480a34f9fb2ebccab5d2b22dc324e7f1a
Lyuboslav Lozanov <lyuboslav.lozanov@abv.bg>
2024-01-25 17:38:06 +0200

    reusing actions
    via Request methods.

--- RoutingForms/frontend/src/App.js
+++ RoutingForms/frontend/src/App.js
@@ -6,11 +6,13 @@ import EventDetailsPage, {
   loader as eventDetailLoader,
   action as deleteEventAction,
 } from "./pages/EventDetails";
-import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
-import EditEventPage, { action as editEventAction } from "./pages/EditEvent";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
+import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 
 const router = createBrowserRouter([
   {
@@ -20,30 +22,24 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: "events",
-        element: <EventsRootLayout />,
+        path: "events", element: <EventsRootLayout />,
         children: [
           { index: true, element: <EventsPage />, loader: eventsLoader },
           {
-            path: ":id",
-            id: "event-details",
-            loader: eventDetailLoader,
+            path: ":id", id: "event-details", loader: eventDetailLoader,
             children: [
-              {
-                index: true,
-                element: <EventDetailsPage />,
-                action: deleteEventAction,
-              },
-              {
-                path: "edit",
-                element: <EditEventPage />,
-                action: editEventAction,
-              },
+              { index: true, element: <EventDetailsPage />, action: deleteEventAction },
+              { path: "edit", element: <EditEventPage />, action: manipulateEventAction },
             ],
           },
-          { path: "new", element: <NewEventPage />, action: newEventAction },
+          { path: "new", element: <NewEventPage />, action: manipulateEventAction },
         ],
       },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction
+      }
     ],
   },
 ]);
--- RoutingForms/frontend/src/pages/EditEvent.js
+++ RoutingForms/frontend/src/pages/EditEvent.js
@@ -1,4 +1,4 @@
-import { useRouteLoaderData, json, redirect } from "react-router-dom";
+import { useRouteLoaderData } from "react-router-dom";
 
 import EventForm from "../components/EventForm";
 
@@ -7,36 +7,9 @@ function EditEventPage() {
   const event = data.event;
   return (
     <>
-      <EventForm event={event} />
+      <EventForm method='PATCH' event={event} />
     </>
   );
 }
 
 export default EditEventPage;
-
-export async function action({ request, params }) {
-  const id = params.id;
-
-  const data = await request.formData();
-
-  const eventData = {
-    title: data.get("title"),
-    image: data.get("image"),
-    description: data.get("description"),
-    date: data.get("date"),
-  };
-
-  const response = await fetch("http://localhost:8080/events/" + id, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(eventData),
-  });
-
-  if (!response.ok) {
-    throw json({ message: "Could not edit event." }, { status: 500 });
-  }
-
-  return redirect("/events/" + id);
-}
