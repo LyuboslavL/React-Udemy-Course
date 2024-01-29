dc7f4b9bbf36828c5b088ef5f92148c343a12801
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2024-01-29 16:16:37 +0200

    register functionality
    By adding action to the Authentication component.

--- EventsAuthentication/frontend/src/components/AuthForm.js
+++ EventsAuthentication/frontend/src/components/AuthForm.js
@@ -8,26 +8,21 @@ function AuthForm() {
 
   return (
     <>
-      <Form method="post" className={classes.form}>
+      <Form method="POST" className={classes.form}>
         <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
         </p>
         <p>
-          <label htmlFor="image">Password</label>
+          <label htmlFor="password">Password</label>
           <input id="password" type="password" name="password" required />
         </p>
-        {!isLogin && (
-          <p>
-            <label htmlFor="image">Confirm Password</label>
-            <input id="repass" type="password" name="password" required />
-          </p>
-        )}
         <div className={classes.actions}>
           <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
-            {isLogin ? "Create new user" : "Login"}
+            {isLogin ? "Don't have an account? \n Register" : "Login"}
           </Link>
+          <button>Save</button>
         </div>
       </Form>
     </>
