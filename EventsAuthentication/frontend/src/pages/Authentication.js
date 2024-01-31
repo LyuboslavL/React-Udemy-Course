c1da9e3382ada0693969940fcb07a7be92cb6cd0
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2024-01-31 13:04:22 +0200

    working registration
    With error in the form when an user is already registered.

--- EventsAuthentication/frontend/src/pages/Authentication.js
+++ EventsAuthentication/frontend/src/pages/Authentication.js
@@ -12,9 +12,9 @@ export async function action({ request }) {
   const searchParams = new URL(request.url).searchParams;
   const mode = searchParams.get("mode") || "login";
 
-  // if (mode !== "login" || mode !== "signup") {
-  //   throw json({ message: "Unsupported mode." }, { status: 422 });
-  // }
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "Unsupported mode." }, { status: 422 });
+  }
 
   const data = await request.formData();
   const authData = {
@@ -24,17 +24,20 @@ export async function action({ request }) {
 
   const response = await fetch("http://localhost:8080/" + mode, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(authData),
   });
 
-  // if (response.status === 422 || response.status === 401) {
-  //   return response;
-  // }
-
-  if (!response.ok) {
-    throw json({ message: "Something went wrong..." }, { status: 500 });
+  if (response.status === 422 || response.status === 401) {
+    return response;
   }
 
+  if (!response.ok) {
+    throw json({ message: "Could not authenticate user." }, { status: 500 });
+  }
+
+  // soon: manage that token
   return redirect("/");
 }
