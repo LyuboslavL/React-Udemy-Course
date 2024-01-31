f2431ac8d9bf527319c4877eab1e39d3e3eee399
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2024-01-31 14:59:59 +0200

    logout functionality
    Clearing the localStorage of the token.

--- EventsAuthentication/frontend/src/App.js
+++ EventsAuthentication/frontend/src/App.js
@@ -16,6 +16,7 @@ import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 import AuthenticationPage, {
   action as authenticationAction,
 } from "./pages/Authentication";
+import { action as logoutAction } from "./pages/Logout";
 
 const router = createBrowserRouter([
   {
@@ -66,6 +67,10 @@ const router = createBrowserRouter([
         path: "auth",
         element: <AuthenticationPage />,
         action: authenticationAction,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
       },
     ],
   },
